778a48b2fe262dfb4ba53428af6e1b407d5555fa
MeyMora <112515882+MeyMora@users.noreply.github.com>
2026-04-02 15:52:18 -0600

    Corrección de error en función crear fila

--- tablero_base.rkt
+++ tablero_base.rkt
@@ -13,7 +13,7 @@
 ; Ejemplo: (crear-fila 4)
 ; resultado: (0 0 0 0)
 ;
-(define (crear-fila n) ; Funcion para crear un tablero
+(define (crear-fila n) ; Funcion para crear una fila 
   (if(= n 0) '() ;pregunta si n es igual a cero, si n es cero devuelve la lista vacia, condicion de parada
      (cons 0(crear-fila(- n 1))) ;construye una nueva lista colocando el elemento 0 al inicio de otra lista.
      ) ;La otra lista se obtiene llamando recursivamente a crear-fila con n-1.
